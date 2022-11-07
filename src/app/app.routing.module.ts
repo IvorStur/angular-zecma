2bd162fb0d20cf63c3b2d6b2283cccb5a86efc03
IvorStur <90836443+IvorStur@users.noreply.github.com>
2022-11-07 09:27:16 +0100

    2 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HelloComponent } from './hello.component';
 import { OneComponent } from './one/one.component';
 import { ThreeComponent } from './three/three.component';
 import { TwoComponent } from './two/two.component';
 
 //const routes: Routes = []; // sets up routes constant where you define your routes
 const routes: Routes = [
-  { path: 'one', component: OneComponent },
+  { path: 'one', component: OneComponent, 
+  children: [
+    {
+      path: 'childa', // child route path
+      component: HelloComponent, // child route component that the router renders
+    },
+    {
+      path: 'childb',
+      component: TwoComponent, // another child route component that the router renders
+    },
+  ],  
+},
   { path: 'two', component: TwoComponent },
   { path: 'three', component: ThreeComponent },
+  { path: '',   redirectTo: '/three', pathMatch: 'full' }, // redirect to `one`
+  { path: '**', component: HelloComponent },
 ];
 // configures NgModule imports and exports
 @NgModule({
